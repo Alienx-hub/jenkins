3def8f1ceb7ba6be6e39895b138355596dad3f1b
Niome Nwaneri <yvette.nwaneri2@gmail.com>
2022-05-12 17:15:11 -0500

    added a new port

--- main.tf
+++ main.tf
@@ -46,6 +46,14 @@ ingress {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "jenkins from public"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
